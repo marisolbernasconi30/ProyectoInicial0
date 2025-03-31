6f9520dc7f0cd3188779c3930a14f6d9abd69c2e
Marisol Bernasconi <usa2018sb1030@gmail.com>
2025-03-31 09:46:38 -0300

    Barra herramientas proyecto

--- src/graficos/BarraHerramientas32.java
+++ src/graficos/BarraHerramientas32.java
@@ -6,67 +6,59 @@ import javax.swing.JFrame;
 
 public class BarraHerramientas32 {
     public static void main (String [] args) {
-         MarcoAccion marcoa=new MarcoAccion();
-        marcoa.setVisible(true);
-        marcoa.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        WFToolBar marcoToolBar=new WFToolBar();
+        marcoToolBar.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marcoToolBar.setVisible(true);
     }
 }
-class MarcoAccion extends JFrame{ 
-	public MarcoAccion() {
+
+class  WFToolBar extends JFrame{ 
+	public WFToolBar() {
+
 		setTitle("Actions test");
 		setBounds(400,150,800,300);
-		PanelAction lamina=new PanelAction();
+		lamina=new JPanel();
 		add(lamina);
-	}
-}
-class PanelAction extends JPanel{
-	
-	public PanelAction () {
-	
-ActionColor accionMagenta=new ActionColor("Magenta", new ImageIcon("bin/graficos/imagenes_java/mag15.png"), Color.MAGENTA);
-ActionColor accionNaranja=new ActionColor("Naranja", new ImageIcon("bin/graficos/imagenes_java/naranja15.png"), Color.ORANGE);
-ActionColor accionVerde=new ActionColor("Verde", new ImageIcon("bin/graficos/imagenes_java/verde15.png"), Color.GREEN);
-	
-		add(new JButton(accionMagenta));
-		add(new JButton(accionNaranja));
-		add(new JButton(accionVerde));
-	/* 
-		JButton botonMagenta=new JButton("Magenta");
-		JButton botonNaranja=new JButton("Naranja");
-		JButton botonVerde=new JButton("Verde");
-		add(botonMagenta);
-		add(botonNaranja);
-		add(botonVerde);
-	*/
-		InputMap mapaEntrada=getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);  //MAPA DE ENTRADA
-		KeyStroke teclaMagenta=KeyStroke.getKeyStroke("ctrl M"); //KeyStroke is a class //COMBINACION DE TECLAS
-		KeyStroke teclaNaranja=KeyStroke.getKeyStroke("ctrl N");
-		KeyStroke teclaVerde=KeyStroke.getKeyStroke("ctrl V");
-	    mapaEntrada.put(teclaMagenta, "fondo_magenta"); 
-	    mapaEntrada.put(teclaNaranja, "fondo_naranja"); 
-	    mapaEntrada.put(teclaVerde, "fondo_verde"); 
-	    
-	    ActionMap mapaAccion=getActionMap();  //MAPA DE ACCION
-	    mapaAccion.put("fondo_magenta", accionMagenta);  //CREAR VINCULO ENTRE EL OBJETO CON LA ACCION
-	    mapaAccion.put("fondo_naranja", accionNaranja);
-	    mapaAccion.put("fondo_verde", accionVerde);
-	
-	}
-	private class ActionColor extends AbstractAction{ //listener class
 
-		public ActionColor (String name, Icon icon, Color color_buttom) {  //construction method
+        Action accionMagenta=new ToolBarAction("Magenta", new ImageIcon("bin/graficos/imagenes_java/mag15.png"), Color.MAGENTA);
+        Action accionNaranja=new ToolBarAction("Naranja", new ImageIcon("bin/graficos/imagenes_java/naranja15.png"), Color.ORANGE);
+        Action accionVerde=new ToolBarAction("Verde", new ImageIcon("bin/graficos/imagenes_java/verde15.png"), Color.GREEN);
+
+        JMenu menu=new JMenu("Colores");
+        menu.add(accionMagenta);
+		menu.add(accionNaranja);
+        menu.add(accionVerde);
+        JMenuBar menu_barra=new JMenuBar();
+        menu_barra.add(menu);
+        setJMenuBar(menu_barra); //agregamos la barra directamente al marco
+
+		//Construccion de la barra de herramientas:
+		JToolBar barra=new JToolBar();
+		barra.add(accionMagenta);
+		barra.add(accionNaranja);
+		barra.add(accionVerde);
+		add(barra, BorderLayout.NORTH);
+	}
+
+	private class ToolBarAction extends AbstractAction{ //listener class
+
+		public ToolBarAction (String name, Icon icon, Color color_buttom) {  //construction method
 			putValue(Action.NAME, name);
 			putValue(Action.SMALL_ICON, icon);
-			putValue(Action.SHORT_DESCRIPTION, "Poner la lamina de color " + name);
-			putValue("background_color", color_buttom);
+			putValue(Action.SHORT_DESCRIPTION, "Color de fondo " + name);
+			putValue("Color", color_buttom);
 		}
 		
 		public void actionPerformed(ActionEvent e) { //for buttons actions
 			
-			Color c=(Color)getValue("background_color"); //getValue returns the value of object events 
-			setBackground(c);
-			System.out.println("Pulsaste el boton: " + getValue(Action.NAME));
-	
+			Color c=(Color)  getValue("Color"); //getValue returns the value of object events 
+			lamina.setBackground(c);
         }
+
 	}
+
+  private JPanel lamina;
+
 }
+
+
