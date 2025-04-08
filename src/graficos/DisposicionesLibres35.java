67a8d9992d58a35b5b92d42971ccd575a8d00eee
Marisol Bernasconi <usa2018sb1030@gmail.com>
2025-04-08 10:34:03 -0300

    disposiciones libres

--- src/graficos/DisposicionesLibres35.java
+++ src/graficos/DisposicionesLibres35.java
@@ -1,6 +1,7 @@
 package graficos;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.LayoutManager;
 public class DisposicionesLibres35 {
    
     public static void main (String [] args){
@@ -21,33 +22,60 @@ class F_W_disposicioneslibre extends JFrame{
     }
 }    
 class LibreSheel extends JPanel{
-       public LibreSheel(){
-        setLayout(null); // con esto, le digo que voy a hacer una disposicion libre
-        JLabel nombre= new JLabel("Inserte su nombre: ");
-        JLabel apellido= new JLabel("Inserte su apellido");
+      
+    public LibreSheel(){
+
+        setLayout(new EnColumnas()); // con esto, le digo que voy a hacer una disposicion libre
+        JLabel nombre= new JLabel("Inserte nombre: ");
+        JLabel apellido= new JLabel("Inserte apellido:");
         JTextField ins_nombre=new JTextField();
         JTextField ins_apellido=new JTextField();
+
         /*nombre.setBounds(20,35,200,20);
         ins_nombre.setBounds(250,35,150,20);
         apellido.setBounds(20, 65, 200, 20);
         ins_apellido.setBounds(250, 65, 150, 20);*/
 
         add(nombre);
-        add(apellido);
         add(ins_nombre);
+        add(apellido);
         add(ins_apellido);
     }
+
+       
 }
 
 class EnColumnas implements LayoutManager{
+    
     public void addLayoutComponent(String name,Component comp){}
-    public void layoutContainer(Container micontenedor){
+    
+    public void layoutContainer(Container micontenedor){ // este metodo, me va a agregar los elementos y ubicarlos
+        
+        int contador=0;  //para saber cuantos elementos agregamos 
 
+        int n=micontenedor.getComponentCount(); //almacena cuantos componentes tiene el contenedor
+        
+        for(int i=0; i<n; i++){  //recorre CADA UNO DE los elementos
+             contador++;
+             Component c=micontenedor.getComponent(i);
+             c.setBounds(x, y, 100, 20);
+             x+=120;
+             
+             if (contador%2==0){  
+                x=20;
+                y+=22;
+             }
+
+        }
     }
-    public Dimension minimumLayoutSize(Container parent){
+    public Dimension minimumLayoutSize(Container parent){  // me los exige LayoutManager
         return null;}
-    public Dimension preferredLayoutSize(Container parent){
+    
+    public Dimension preferredLayoutSize(Container parent){  // me los exige LayoutManager
         return null;}
-    public void removeLayoutComponent(Component comp){}
+    
+    public void removeLayoutComponent(Component comp){}  // me los exige LayoutManager
 
+    private int y=10;
+    private int x=20;
 }
